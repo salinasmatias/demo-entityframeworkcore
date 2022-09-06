b5a2629b621d607dad4085846b12a82e11ab3899
Matias <salinasmatias1990@gmail.com>
2022-09-06 04:34:42 -0300

    Setting up environment

---
+++ src/Presentation/Program.cs
@@ -0,0 +1,20 @@
+﻿using Presentation.Models;
+using System;
+using System.Linq;
+
+namespace Presentation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new NorthwindContext())
+            {
+                foreach (var item in context.AlphabeticalListOfProducts)
+                {
+                    Console.WriteLine(item.ProductName);
+                }
+            }
+        }
+    }
+}
